bd5f5953122de55b501edcf4bf9d524e08ccd7ed
Jacopo Grazzini <gjacopo@gmail.com>
2017-03-12 20:39:18 +0100

    updated methods

--- prophet/README.md
+++ prophet/README.md
@@ -6,9 +6,11 @@ Applying Facebook `Prophet` model for forecasting ESTAT monthly indicators
 
 **About**
 
-This is a **blind/dummy** (no assumption whatsoever) application of  [`Prophet`](https://facebookincubator.github.io/prophet/) automatic procedure for forecast estimates of ESTAT [*tour_occ_nim*](http://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=tour_occ_nim&lang=en) time-series on the number of *"nights spent at tourist accommodation establishments"*) per month.
+This is a **blind/dummy** (no assumption whatsoever) application of  [`Prophet`](https://facebookincubator.github.io/prophet/) automatic procedure for forecast estimates of ESTAT [*tour_occ_nim*](http://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=tour_occ_nim&lang=en) time-series on the number of *"nights spent at tourist accommodation establishments"* per month.
 
 **Description**
+
+*(from the source [webpage](https://research.fb.com/prophet-forecasting-at-scale/))*
 
 At its core, the [`Prophet`](https://facebookincubator.github.io/prophet/) procedure is an additive regression model with four main components (using [`Stan`](http://mc-stan.org/) Bayesian approach, see reference [below](#Reference)):
 * a piecewise linear (or logistic) growth curve trend: Prophet automatically detects changes in trends by selecting changepoints from the data,
@@ -16,14 +18,15 @@ At its core, the [`Prophet`](https://facebookincubator.github.io/prophet/) proce
 * a weekly seasonal component using dummy variables,
 * a user-provided list of important holidays.
 
-In practice, non-linear trends are fit with yearly and weekly seasonality (plus holidays). `Prophet` is robust to missing data, shifts in the trend, and large outliers.
+In practice, non-linear trends are fit with yearly and weekly seasonality (plus holidays). The method is also robust to missing data, shifts in the trend, and large outliers.
 
 **Usage**
 
-Facebook has open sourced  [`Prophet software`](https://github.com/facebookincubator/prophet), a forecasting project with an interface available in `Python` ([here](https://pypi.python.org/pypi/fbprophet/)). We will use this resource. 
+Facebook has open sourced  [`Prophet software`](https://github.com/facebookincubator/prophet), a forecasting project with an [interface](https://pypi.python.org/pypi/fbprophet/) available in `Python`. We will use this resource. 
 
-Run the `tour_forecast.py` source code or explore the `tour_forecast.ipynb` notebook to produce the following forecast estimates of ESTAT [*tour_occ_nim*] monthly indicator:
-<img src="tour_occ_nim_predict.png" alt="prediction" width="400">
+Run the [`tour_forecast.py`](tour_forecast.py) source code or explore the [`tour_forecast.ipynb`](tour_forecast.ipynb) notebook to produce the following forecast estimates of ESTAT *tour_occ_nim* monthly indicator:
+
+<img src="tour_occ_nim_predict.png" alt="prediction" width="800">
 
 **<a name="Reference"></a>Reference**
 
--- prophet/tour_forecast.py
+++ prophet/tour_forecast.py
@@ -14,10 +14,7 @@ import pandas as pd
 import numpy as np#analysis:ignore
 from matplotlib import pyplot as plt
 
-try:
-    from fbprophet import Prophet
-except:
-    raise IOError('fprophet package is required')
+from fbprophet import Prophet
     
 PROTOCOL        = "http"
 API_LANG        = "en"
@@ -28,36 +25,19 @@ API_URL         = "{}://{}/rest/data/v{}/{}/{}".format(
                   PROTOCOL, API_DOMAIN, API_VERS, API_FMT, API_LANG
                   )
 
-GEO             = "EU28"
-# TIME : all
-
-# price indexes
-INDICATOR       = "prc_hicp_midx"
-CODE            = "TOT_X_FUEL" #"CP00"
-INDEX           = "I15"
-URL             = "{}/{}?geo={}&coicop={}&unit={}".format(
-                  API_URL, INDICATOR, GEO, CODE, INDEX)
-
-# unemployment
-INDICATOR       = "une_rt_m"
-UNIT            = "THS_PER"
-AGE             = "TOTAL"
-SEX             = "T"
-S_ADJ           = "SA"
-
-URL             = "{}/{}?geo={}&unit={}&age={}&sex={}&s_adj={}".format(
-                  API_URL, INDICATOR, GEO, UNIT, AGE, SEX, S_ADJ)
-
-# tour accomodation
-# http://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=tour_occ_nim&lang=en
-INDICATOR       = (u'tour_occ_nim', "Tour accomodation")
-UNIT            = (u'NR', "Number of nights")
-NACE_R2         = (u'I551', "Hotels; holiday and other short-stay accommodation...")
-INDIC_TO        = (u'B006', "Nights spent, total")
-
-URL             = "{}/{}?geo={}&unit={}&nace_r2={}&indic_to={}".format(
-                  API_URL, INDICATOR[0], GEO, UNIT[0], NACE_R2[0], INDIC_TO[0])
-
+def build_url(indicator, **kwargs):
+    url = "{}/{}?".format(API_URL, indicator[0])
+    if 'geo' in kwargs:
+        url = "{}geo={}&".format(url, kwargs.pop('geo', None))
+    if 'time' in kwargs:
+        url = "{}time={}&".format(url, kwargs.pop('time', None))
+    #_izip_replicate = lambda d : [[(k,i) for i in d[k]] if isinstance(d[k], (tuple,list))        \
+    #        else (k, d[k])  for k in d]    
+    _no_replicate = lambda d : d.items()
+    filters = '&'.join(['{k}={v}'.format(k=k, v=v[0]) for (k, v) in _no_replicate(kwargs)])
+    url = "{}{}".format(url, filters)
+    return url
+    
 def get_response(url):
     # request the URL
     session = requests.session()
@@ -82,7 +62,6 @@ def get_response(url):
        
     return resp
     
-
 def build_dataframe(resp):
     lbl2idx = resp['dimension']['time']['category']['index']
     idx2lbl = {v:k for (k,v) in lbl2idx.items()}
@@ -96,29 +75,49 @@ def build_dataframe(resp):
                  for (k,v) in data.items()}
         # instead we will use pandas.datetime below
     except:
-        table = {k.replace('M','-'):v for (k,v) in data.items()}
-    
-    # the input to Prophet is always a dataframe with two columns: ds and y. 
+        table = {k.replace('M','-'):v for (k,v) in data.items()}    
     # create data frame with columns ds (date type) and y, the time series.
     df = pd.DataFrame(list(table.items()), columns=['ds','y'])
     # df = df[df['y'].notnull()]
-     
-    #fig = df.sort_values('ds').plot(x='ds',y='y')
-    #fig.set_title("Historical data")
-    
-    df.sort_values('ds', inplace=True)
-    df.head(); df.tail()
-    
+    df.sort_values('ds', inplace=True)    
     ds_last = df['ds'].values[-1]
-
     df['ds'] = pd.to_datetime(df['ds'])
     return df, ds_last
-    
-resp = get_response(URL)
+
+def predict_prophet(df, nyears):
+    # we fit the model by instantiated a new Prophet object.
+    m = Prophet(growth = "linear", yearly_seasonality=True, weekly_seasonality=False)
+    # we call its fit method and pass in the historical dataframe
+    m.fit(df)    
+    # we extend into the future a specified number of days
+    future = m.make_future_dataframe(periods=12*nyears, freq='M')
+    fcst = m.predict(future)
+    return m, fcst
+
+
+## input data loading and formatting
+
+INDICATOR       = (u'tour_occ_nim', "Tour accomodation")
+# http://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=tour_occ_nim&lang=en
+GEO             = "EU28"
+# TIME          = all
+
+FILTERS       = {'unit': (u'NR', "Number of nights"),
+                'nace_r2': (u'I551', "Hotels; holiday and other short-stay accommodation..."),
+                'indic_to': (u'B006', "Nights spent, total")
+                }
+                
+url = build_url(INDICATOR, geo=GEO, **FILTERS)
+      
+#URL             = "{}/{}?geo={}&unit={}&nace_r2={}&indic_to={}".format(
+#                  API_URL, INDICATOR[0], GEO, UNIT[0], NACE_R2[0], INDIC_TO[0])
+  
+resp = get_response(url)
 df, ds_last = build_dataframe(resp)  
+df.head(); df.tail()
 
 xlabel = "Time"
-ylabel = "{} : {} - {}".format(INDICATOR[0], INDIC_TO[1], GEO)  
+ylabel = "{} : {} - {}".format(INDICATOR[0], FILTERS['indic_to'][1], GEO)  
 fig = plt.figure(facecolor='w', figsize=(10, 6))
 ax = fig.add_subplot(111)
 ax.plot(df['ds'], df['y'], 'k.')
@@ -127,45 +126,19 @@ ax.grid(True, which='major', c='gray', ls='-', lw=1, alpha=0.2)
 ax.set_xlabel(xlabel, fontsize=14); ax.set_ylabel(ylabel, fontsize=14)
 fig.suptitle("Historical data (last: {})".format(ds_last), fontsize=16)
 fig.tight_layout()
-
-#From the source [webpage](https://research.fb.com/prophet-forecasting-at-scale/):
-
-#At its core, the Prophet procedure is an additive regression model with four main components:
-
-#A piecewise linear or logistic growth curve trend. Prophet automatically detects changes in trends by selecting changepoints from the data.
-#A yearly seasonal component modeled using Fourier series.
-#A weekly seasonal component using dummy variables.
-#A user-provided list of important holidays.
-
-
-# Stan http://mc-stan.org
-NYEARS=5
-
-def predict_prophet(df, nyears):
-    # we fit the model by instantiated a new Prophet object.
-    m = Prophet(growth = "linear", yearly_seasonality=True, weekly_seasonality=False)
-    # we call its fit method and pass in the historical dataframe. 
-    # fit a non-linear additive model
-    m.fit(df)
     
-    # we extend into the future a specified number of days using the helper method Prophet.make_future_dataframe
-    future = m.make_future_dataframe(periods=12*nyears, freq='M')
-    fcst = m.predict(future)
-    # fcst[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
-    return m, fcst
-    
+## forecast configuration and estimation
+
+NYEARS          = 5
     
 m, fcst = predict_prophet(df, NYEARS)
+# fcst[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
 
 fig = m.plot(fcst, uncertainty=True) 
 plt.axvline(pd.to_datetime(ds_last), color='r', linestyle='--', lw=2)
 plt.xlabel(xlabel, fontsize=14); plt.ylabel(ylabel, fontsize=14)
 fig.suptitle("Forecast data ({} years)".format(NYEARS), fontsize=16)
-#plt.xlabel('xlabel')
-#plt.ylabel('ylabel')
-fig.savefig('tour_occ_nim_predict.png')
-    
+# fig.savefig('tour_occ_nim_predict.png')  
 fig = m.plot_components(fcst, uncertainty=True);
 fig.suptitle("Forecast components", fontsize=16)
 
-if __name__ == "__main__":
