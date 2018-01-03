182745dc603388cc36ca8dfaff3e99b652acfb0e
Jacopo Grazzini <gjacopo@gmail.com>
2018-01-03 14:09:32 +0100

    Add files via upload

--- estatref/csvimporter.java
+++ estatref/csvimporter.java
@@ -1,6 +1,18 @@
-/*
+/**
+## csvimporter.java {#java_csvimporter}
+Imports simple CSV file of bibliographical references into JabRef
 
-http://help.jabref.org/en/CustomImports
+### Notes
+1. Every field in the input CSV file must be separated by a comma.
+2. This implementation follows the instructions/examples of original JabRef
+documentation: http://help.jabref.org/en/CustomImports
+3. All BibTex entry types are listed in the source file: 
+https://github.com/JabRef/jabref/blob/master/src/main/java/org/jabref/model/entry/BibtexEntryTypes.java
+and appears as follows:
+
+	ALL = Arrays.asList(ARTICLE, INBOOK, BOOK, BOOKLET, INCOLLECTION, CONFERENCE,
+	INPROCEEDINGS, PROCEEDINGS, MANUAL, MASTERSTHESIS, PHDTHESIS, TECHREPORT, 
+	UNPUBLISHED, MISC)
  */
 
 import java.io.BufferedReader;
@@ -14,7 +26,7 @@ import net.sf.jabref.logic.util.FileExtensions;
 import net.sf.jabref.model.entry.BibEntry;
 import net.sf.jabref.model.entry.BibtexEntryTypes;
 
-public class SimpleCSVImporter extends Importer {
+public class CSVImporter extends Importer {
 
     @Override
     public String getName() {
@@ -28,7 +40,7 @@ public class SimpleCSVImporter extends Importer {
 
     @Override
     public String getDescription() {
-        return "Imports CSV files - Every field is separated by a semicolon";
+        return "Imports CSV files - Every field is separated by a comma";
     }
 
     @Override
@@ -40,15 +52,39 @@ public class SimpleCSVImporter extends Importer {
     public ParserResult importDatabase(BufferedReader input) throws IOException {
         List<BibEntry> bibitems = new ArrayList<>();
 
-        String line = input.readLine();
+        String line;
+		// skip the 4 first lines
+		for (int i=1;i<=4;i++) 
+			line input.readLine();
+		// read...
         while (line != null) {
             if (!line.trim().isEmpty()) {
-                String[] fields = line.split(";");
+                String[] fields = line.split(",");
                 BibEntry be = new BibEntry();
-                be.setType(BibtexEntryTypes.TECHREPORT);
-                be.setField("year", fields[0]);
-                be.setField("author", fields[1]);
-                be.setField("title", fields[2]);
+				if (String(fields[4]).equals("Scientific journal")) {
+					be.setType(BibtexEntryTypes.ARTICLE);
+				} else if(String(fields[4]).equals("Book or chapter in book")) {
+					be.setType(BibtexEntryTypes.INBOOK);
+				} else if(String(fields[4]).equals("Public working paper")) {
+					be.setType(BibtexEntryTypes.TECHREPORT);
+				} else if(String(fields[4]).equals("Conference proceedings")) {
+					be.setType(BibtexEntryTypes.INPROCEEDINGS);
+				} else {
+					be.setType(BibtexEntryTypes.MISC);
+				}
+				be.setField("project", 		fields[0]);
+				// be.setField("institution", 	fields[0]);
+                be.setField("dataset", 		fields[1]);
+                be.setField("title", 		fields[2]);
+                be.setField("howpublished", fields[4]);
+                be.setField("author", 		fields[7]);
+                be.setField("keywords", 	fields[8]);
+                be.setField("abstract", 	fields[9]);
+                be.setField("language", 	fields[10]);
+                be.setField("year", 		fields[11]);
+                be.setField("doi", 			fields[13]);
+                be.setField("url", 			fields[14]);
+                // be.setField("summary", 		fields[17]);
                 bibitems.add(be);
                 line = input.readLine();
             }
